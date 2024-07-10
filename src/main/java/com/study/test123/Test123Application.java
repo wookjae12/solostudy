82523499bb1c0e1f90c25fdec0afeb42707a9ee9
wj.jang <wj.jang>
2024-07-10 16:06:26 +0900

    처음올리기

---
+++ src/main/java/com/study/test123/Test123Application.java
@@ -0,0 +1,13 @@
+package com.study.test123;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test123Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test123Application.class, args);
+	}
+
+}
